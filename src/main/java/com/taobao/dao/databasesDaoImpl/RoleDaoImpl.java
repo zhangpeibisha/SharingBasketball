d64c83025d83861ca0f6a599c767e51631136d3e
zhangpei <zhangpe0312@qq.com>
2018-02-26 02:59:59 +0800

    删除了测试文件，现在系统可以正常工作

--- src/main/java/com/taobao/dao/databasesDaoImpl/RoleDaoImpl.java
+++ src/main/java/com/taobao/dao/databasesDaoImpl/RoleDaoImpl.java
@@ -1,14 +1,25 @@
 package com.taobao.dao.databasesDaoImpl;
 
-import com.taobao.dao.base.BaseImplDao;
+import com.taobao.dao.databasesDao.SupperBaseDAOImp;
 import com.taobao.dao.entity.Role;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 /**
  * Create by zhangpe0312@qq.com on 2018/2/26.
  */
 @Service
-public class RoleDaoImpl extends BaseImplDao<Role>{
+public class RoleDaoImpl extends SupperBaseDAOImp<Role>{
 
+    @Override
+    public <T> List<T> findByCriteria(T object, Integer startRow, Integer pageSize) {
+        return null;
+    }
+
+    @Override
+    public <T> Long findByCriteriaCount(T object) {
+        return null;
+    }
 }

8cbfdd492bea0aeab295ed59b6a1786d41082f50
zhangpei <zhangpe0312@qq.com>
2018-02-27 14:42:36 +0800

    创建了篮球的开发接口类和充值开发接口类

--- src/main/java/com/taobao/web/control/BasketballControl.java
+++ src/main/java/com/taobao/web/control/BasketballControl.java
@@ -1,12 +1,40 @@
 package com.taobao.web.control;
 
+import com.taobao.dao.databasesDaoImpl.BasketballDaoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Create by zhangpe0312@qq.com on 2018/2/26.
- *
+ * <p>
  * 关于篮球的一些使用接口
  */
 @Controller
 public class BasketballControl {
+
+    @Autowired
+    private BasketballDaoImpl basketballDao;
+
+    /**
+     * 返回可以租借、并且完好的篮球列表
+     *
+     * @param req
+     * @return
+     */
+    @RequestMapping(name = "/basketballList", method = RequestMethod.GET)
+    public @ResponseBody
+    Map<String, Object> basketballList(HttpServletRequest req) {
+        Map<String, Object> map = new HashMap<>();
+
+        return map;
+
+    }
+
 }
---
+++ src/main/java/com/taobao/web/control/MoneyControl.java
@@ -0,0 +1,12 @@
+package com.taobao.web.control;
+
+/**
+ * Create by zhangpe0312@qq.com on 2018/2/27.
+ *
+ * 控制金额的扣除、充值、返回等操作
+ */
+public class MoneyControl {
+
+
+
+}

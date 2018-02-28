59a87d6fa04b7304ab492195ee797a93c9472683
tangdaijia <253141924@qq.com>
2018-02-28 22:03:37 +0800

    篮球列表 租赁列表

--- src/main/webapp/js/basketList.js
+++ src/main/webapp/js/basketList.js
@@ -1,88 +1,109 @@
-var listUrl = "http://localhost:8080/rentList.do";
-var pageLimit = 10;
-var currentPage = 1;
+$(document).ready(function () {
+    var listUrl = "";
+    var pageLimit = 10;
+    var currentPage = 1;
 
+    function init() {
+        $.ajax({
+            type: 'GET',
+            url: listUrl,
+            data: {
+                limit: pageLimit,
+                currentPage: currentPage
+            },
+            success: function (data) {
+                if(data.data==0){
+                    var listData = data.basketballs;
+                    var total = data.total;
+                    showData(listData);
 
-function init() {
-    $.ajax({
-        type: 'GET',
-        url: listUrl,
-        data: {
-            limit: pageLimit,
-            currentPage: currentPage
-        },
-        success: function (data) {
-            console.info(data);
-            if(data.data==0){
-                var listData = data.basketballs;
-                showData(data.total,listData);
-            }
-            else if(data.data==1)
-                alert("查询成功但没有篮球！");
-            else if(data.data==2)
-                alert("error！");
-        },
-        dataType: "json"
-    });
-}
-
-function showData(total,listData) {
-    var temp = [], showNum = listData.length;
-
-    temp.push('<table class="table table-hover">');
-    temp.push('<thead><tr><th>篮球编号</th><th>篮球型号</th><th>是否可借</th>' +
-        '<th>租借押金</th><th>小时租金</th><th>操作</th></tr><tbody>');
-    for (var i = 0; i < showNum; i++) {
-        if(listData[i].isRent=="0")
-            listData[i].isRent ="可借";
-        else listData[i].isRent ="不可借";
-        temp.push("<tr><td>" + listData[i].basketballID + "</td><td>" + listData[i].model + "</td><td>"
-            + listData[i].isRent + "</td><td>"+ listData[i].rent.deposit + "</td><td>"
-            + listData[i].rent.billing + "</td><td>"
-            + "<a class='rent-a'>租借</a>" + "</td></tr>");
-    }
-    temp.push('</tbody></table>');
-
-    $('#list').html(temp.join(''));
-
-    $('#page').bootstrapPaginator({
-        bootstrapMajorVersion: 3,
-        currentPage: 1,//当前页码
-        totalPages: total,
-        numberOfPages: pageLimit,//一页显示几个按钮
-        itemTexts: function (type, page, current) {
-            switch (type) {
-                case "first": return "首页";
-                case "prev": return "上一页";
-                case "next": return "下一页";
-                case "last": return "末页";
-                case "page": return page;
-            }
-        },//改写分页按钮字样
-        onPageClicked: function (event, originalEvent, type, page) {
-            $.ajax({
-                url: listUrl,
-                type: 'GET',
-                data: {
-                    limit: pageLimit,
-                    currentPage: page
-                },
-                dataType: 'json',
-                success: function (data) {
-                    console.info(data);
-                    if(data.data==0){
-                        var listData = data.basketballs;
-                        showData(data.total,listData);
-                    }
-                    else if(data.data==1)
-                        alert("查询成功但没有篮球！");
-                    else if(data.data==2)
-                        alert("error！");
+                    var num = (total+pageLimit -1)/pageLimit;//向上取整
+                    $('#page').bootstrapPaginator({
+                        bootstrapMajorVersion: 3,
+                        currentPage: 1,//当前页码
+                        totalPages: num,
+                        numberOfPages: 5,
+                        shouldShowPage:true,//是否显示该按钮
+                        itemTexts: function (type, page, current) {
+                            switch (type) {
+                                case "first": return "首页";
+                                case "prev": return "上一页";
+                                case "next": return "下一页";
+                                case "last": return "末页";
+                                case "page": return page;
+                            }
+                        },
+                        onPageClicked: function (event, originalEvent, type, page) {
+                            $.ajax({
+                                url: listUrl,
+                                type: 'GET',
+                                data: {
+                                    limit: pageLimit,
+                                    currentPage: page
+                                },
+                                dataType: 'json',
+                                success: function (data) {
+                                    console.info(data);
+                                    if(data.data==0){
+                                        var listData = data.basketballs;
+                                        showData(listData);
+                                    }
+                                    else if(data.data==1){
+                                        noData();
+                                    }
+                                    else if(data.data==2)
+                                        alert("error！");
+                                }
+                            });
+                        }
+                    });
                 }
-            });
+                else if(data.data==1){
+                    noData();
+                }
+                else if(data.data==2)
+                    alert("error！");
+            },
+            dataType: "json"
+        });
+    }
 
+    function showData(listData) {
+        var temp = [], showNum = listData.length;
+
+        temp.push('<table class="table table-hover">');
+        temp.push('<thead><tr><th>篮球编号</th><th>篮球型号</th><th>压力标准值</th>' +
+            '<th>当前压力值</th><th>是否损坏</th><th>是否可借</th><th>租借规则</th>'+
+            '<th>租借押金</th><th>小时租金</th></tr><tbody>');
+        for (var i = 0; i < showNum; i++) {
+            if(listData[i].isRent=="0")
+                listData[i].isRent ="可借";
+            else listData[i].isRent ="不可借";
+            if(listData[i].isBad=="0")
+                listData[i].isBad ="正常";
+            else listData[i].isBad ="损坏";
+            temp.push("<tr><td>" + listData[i].basketballID + "</td><td>" + listData[i].model + "</td><td>"
+                + listData[i].pressure+ "</td><td>" + listData[i].nowPressure + "</td><td>"
+                + listData[i].isBad + "</td><td>" + listData[i].isRent + "</td><td>"
+                + listData[i].rent + "</td><td>"+ listData[i].rent.deposit + "</td><td>"
+                + listData[i].rent.billing + "</td><td>");
         }
-    });
-}
+        temp.push('</tbody></table>');
 
-init();
+        $('#list').html(temp.join(''));
+    }
+
+    function noData() {
+        var temp = [];
+        temp.push('<table class="table table-hover">');
+        temp.push('<thead><tr><th>篮球编号</th><th>篮球型号</th><th>压力标准值</th>' +
+        '<th>当前压力值</th><th>是否损坏</th><th>是否可借</th><th>租借规则</th>'+
+        '<th>租借押金</th><th>小时租金</th></tr><tbody>');
+        temp.push("<tr><td colspan='9' style='text-align: center'>暂无数据</td></tr>");
+        temp.push('</tbody></table>');
+
+        $('#list').html(temp.join(''));
+    }
+
+    init();
+});
--- src/main/webapp/js/rent.js
+++ src/main/webapp/js/rent.js
@@ -13,21 +13,63 @@ $(document).ready(function () {
                 currentPage: currentPage
             },
             success: function (data) {
-                console.info(data);
                 if(data.data==0){
                     var listData = data.basketballs;
-                    showData(data.total,listData);
+                    var total = data.total;
+                    showData(listData);
+
+                    var num = (total+pageLimit -1)/pageLimit;//向上取整
+                    $('#page').bootstrapPaginator({
+                        bootstrapMajorVersion: 3,
+                        currentPage: 1,//当前页码
+                        totalPages: num,
+                        numberOfPages: 5,
+                        shouldShowPage:true,//是否显示该按钮
+                        itemTexts: function (type, page, current) {
+                            switch (type) {
+                                case "first": return "首页";
+                                case "prev": return "上一页";
+                                case "next": return "下一页";
+                                case "last": return "末页";
+                                case "page": return page;
+                            }
+                        },
+                        onPageClicked: function (event, originalEvent, type, page) {
+                            $.ajax({
+                                url: listUrl,
+                                type: 'GET',
+                                data: {
+                                    limit: pageLimit,
+                                    currentPage: page
+                                },
+                                dataType: 'json',
+                                success: function (data) {
+                                    console.info(data);
+                                    if(data.data==0){
+                                        var listData = data.basketballs;
+                                        showData(listData);
+                                    }
+                                    else if(data.data==1){
+                                        noData();
+                                    }
+                                    else if(data.data==2)
+                                        alert("error！");
+                                }
+                            });
+                        }
+                    });
                 }
-                else if(data.data==1)
-                    alert("查询成功但没有篮球！");
+                else if(data.data==1){
+                    noData();
+                }
                 else if(data.data==2)
                     alert("error！");
             },
             dataType: "json"
         });
     }
-
-    function showData(total,listData) {
+    
+    function showData(listData) {
         var temp = [], showNum = listData.length;
 
         temp.push('<table class="table table-hover">');
@@ -45,45 +87,17 @@ $(document).ready(function () {
         temp.push('</tbody></table>');
 
         $('#list').html(temp.join(''));
+    }
 
-        $('#page').bootstrapPaginator({
-            bootstrapMajorVersion: 3,
-            currentPage: 1,//当前页码
-            totalPages: total,
-            numberOfPages: pageLimit,//一页显示几个按钮
-            itemTexts: function (type, page, current) {
-                switch (type) {
-                    case "first": return "首页";
-                    case "prev": return "上一页";
-                    case "next": return "下一页";
-                    case "last": return "末页";
-                    case "page": return page;
-                }
-            },//改写分页按钮字样
-            onPageClicked: function (event, originalEvent, type, page) {
-                $.ajax({
-                    url: listUrl,
-                    type: 'GET',
-                    data: {
-                        limit: pageLimit,
-                        currentPage: page
-                    },
-                    dataType: 'json',
-                    success: function (data) {
-                        console.info(data);
-                        if(data.data==0){
-                            var listData = data.basketballs;
-                            showData(data.total,listData);
-                        }
-                        else if(data.data==1)
-                            alert("查询成功但没有篮球！");
-                        else if(data.data==2)
-                            alert("error！");
-                    }
-                });
+    function noData() {
+        var temp = [];
+        temp.push('<table class="table table-hover">');
+        temp.push('<thead><tr><th>篮球编号</th><th>篮球型号</th><th>是否可借</th>' +
+            '<th>租借押金</th><th>小时租金</th><th>操作</th></tr><tbody>');
+        temp.push("<tr><td colspan='6' style='text-align: center'>暂无数据</td></tr>");
+        temp.push('</tbody></table>');
 
-            }
-        });
+        $('#list').html(temp.join(''));
     }
 
     init();
